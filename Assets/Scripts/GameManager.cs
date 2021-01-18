3ee00e402908600b797954d660ef890204c3d9e9
frederik desmet <info@neo-level.com>
2021-01-18 17:45:56 +0100

    Created a coroutine to spawn object.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    
+    private float _spawnRate = 1.0f;
+
     // Start is called before the first frame update
-   private void Start()
+    private void Start()
     {
-        
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     private void Update()
     {
-        
+    }
+
+    private IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(_spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
     }
 }
