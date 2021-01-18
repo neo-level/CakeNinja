3e7cf7cd3d77d539a2c3d09f29dac8fef85ca59c
frederik desmet <info@neo-level.com>
2021-01-18 17:12:35 +0100

    Added code to the script that tosses an object randomly in the air.

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    private Rigidbody _targeyRigidbody;
+    private float _torqueRange = 10.0f;
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        // Get the rigidbody component on the object.
+        _targeyRigidbody = GetComponent<Rigidbody>();
+
+        ApplyRandomUpwardForce();
+        ApplyRandomRotation();
+
+        // Random position.
+        transform.position = new Vector3(
+            x: Random.Range(-4, 4),
+            y: -6,
+            z: 0);
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    // Adds a random rotation on the object.
+    private void ApplyRandomRotation()
+    {
+        _targeyRigidbody.AddTorque(
+            x: Random.Range(-_torqueRange, _torqueRange),
+            y: Random.Range(-_torqueRange, _torqueRange),
+            z: Random.Range(-_torqueRange, _torqueRange), ForceMode.Impulse);
+    }
+
+    // Adds an upward force between a random value.
+    private void ApplyRandomUpwardForce()
+    {
+        _targeyRigidbody.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
     }
 }
