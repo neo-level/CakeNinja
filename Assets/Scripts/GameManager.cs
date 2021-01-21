6848c9a151b2a43c6100da6ec56e6b302c69938d
frederik desmet <info@neo-level.com>
2021-01-21 14:03:56 +0100

    Created a method that updates the score.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,16 +9,15 @@ public class GameManager : MonoBehaviour
     public TextMeshProUGUI scoreText;
 
     private int _score;
+
     private float _spawnRate = 1.0f;
+
     // Start is called before the first frame update
     private void Start()
     {
         // Set score to 0 on start of the game.
-        _score = 0;
+        UpdateScore(0);
 
-        // Set the content of the text.
-        scoreText.text = "Score: " + _score;
-        
         StartCoroutine(SpawnTarget());
     }
 
@@ -34,6 +33,21 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(_spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
+            // Temp increment score on spawn to test if it works.
+            UpdateScore(5);
         }
+    }
+
+    /// <summary>
+    /// Updates the score by adding a value for every object that spawns => phase 1
+    /// </summary>
+    /// <param name="scoreToAdd"></param>
+    private void UpdateScore(int scoreToAdd)
+    {
+        // Add the score.
+        _score += scoreToAdd;
+
+        // Set the content of the text.
+        scoreText.text = "Score: " + _score;
     }
 }
