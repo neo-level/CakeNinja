11e41789480f5afe7f0c27528049c554d8abe839
frederik desmet <info@neo-level.com>
2021-01-21 17:19:11 +0100

    Fetch the button component and store it in a variable.

--- Assets/Scripts/DifficultyButton.cs
+++ Assets/Scripts/DifficultyButton.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// Enables the interaction with the UI, for this specific the buttons.
+using UnityEngine.UI;
 
 public class DifficultyButton : MonoBehaviour
 {
+    public Button button;
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        button = GetComponent<Button>();
     }
 
     // Update is called once per frame
