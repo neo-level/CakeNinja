f72e61ee03425e66645d4a655341178d904ccf52
frederik desmet <info@neo-level.com>
2021-01-21 13:56:10 +0100

    Set a variable to store the ui component in. set the content of that component for a start value.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
-    private float _spawnRate = 1.0f;
+    public TextMeshProUGUI scoreText;
 
+    private int _score;
+    private float _spawnRate = 1.0f;
     // Start is called before the first frame update
     private void Start()
     {
+        // Set score to 0 on start of the game.
+        _score = 0;
+
+        // Set the content of the text.
+        scoreText.text = "Score: " + _score;
+        
         StartCoroutine(SpawnTarget());
     }
 
